b17d542020d8c3d850980c580d48e40e0909d3c1
Emanuel Cruz <emanuelcruz091@gmail.com>
2024-03-25 16:37:28 -0300

    config: custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.html",],
+  content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "c-white": "hsl(0, 0%, 100%)",
+        "c-light-grayish-violet": "hsl(270, 3%, 87%)",
+        "c-dark-grayish-violet": "hsl(279, 6%, 55%)",
+        "c-very-dark-violet": "hsl(278, 68%, 11%)",
+      },
+      fontSize: {
+        custom: "18px",
+      },
+
+      fontFamily: {
+        main: "space-grotesk",
+      },
+    },
   },
   plugins: ["prettier-plugin-tailwindcss"],
-}
-
+};
